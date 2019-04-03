276cd8fc98e93913766641e7e2f4896f91d06b5b
Marcelo Modesto Costa <marcelomodesto@gmail.com>
2019-04-03 10:33:25 -0300

    Acertos no arquivo de configuracao do wildfly em funcao da mudanca para o Gradle Builder

---
+++ src/br/usp/ime/ganimedes/model/Anunciante.java
@@ -0,0 +1,192 @@
+package br.usp.ime.ganimedes.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+@ManagedBean(name = "AnuncianteLogado")
+@SessionScoped
+
+@Entity
+@Table(name = "ANUNCIANTE")
+public class Anunciante implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@Column(unique = true)
+	private String codlog;
+
+	private String nome;
+	private String senha;
+	private String salt;
+
+	private boolean ativado = false;
+
+	@Column(unique = true)
+	private String cpf;
+
+	private String telefone;
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Anuncio> anuncios = new ArrayList<Anuncio>();
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCodlog() {
+		return codlog;
+	}
+
+	public void setCodlog(String codlog) {
+		this.codlog = codlog;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	public boolean isAtivado() {
+		return ativado;
+	}
+
+	public void setAtivado(boolean ativado) {
+		this.ativado = ativado;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public List<Anuncio> getAnuncios() {
+		return anuncios;
+	}
+
+	public void setAnuncios(List<Anuncio> anuncios) {
+		this.anuncios = anuncios;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((anuncios == null) ? 0 : anuncios.hashCode());
+		result = prime * result + (ativado ? 1231 : 1237);
+		result = prime * result + ((codlog == null) ? 0 : codlog.hashCode());
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((salt == null) ? 0 : salt.hashCode());
+		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
+		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Anunciante other = (Anunciante) obj;
+
+		if (ativado != other.ativado)
+			return false;
+		if (codlog == null) {
+			if (other.codlog != null)
+				return false;
+		} else if (!codlog.equals(other.codlog))
+			return false;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (salt == null) {
+			if (other.salt != null)
+				return false;
+		} else if (!salt.equals(other.salt))
+			return false;
+		if (senha == null) {
+			if (other.senha != null)
+				return false;
+		} else if (!senha.equals(other.senha))
+			return false;
+		if (telefone == null) {
+			if (other.telefone != null)
+				return false;
+		} else if (!telefone.equals(other.telefone))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Anunciante [id=" + id + ", codlog=" + codlog + ", nome=" + nome + ", senha=" + senha + ", salt=" + salt + ", ativado=" + ativado + ", cpf=" + cpf + ", telefone=" + telefone  + "]";
+	}
+
+}
